93e09f237ea4aa7dee11cce842041ed4ff28c13a
Max Shilling <maxshilling@Maxs-MacBook-Pro.local>
2018-02-26 10:54:58 +0000

    ?

---
+++ data3essay1.R
@@ -0,0 +1,15 @@
+# data3essay
+
+##### Packages #####
+library(tidyverse)
+library(data.table)
+library(readr)
+library(readxl)
+
+##### github #####
+cc
+
+##### Loading data #####
+
+w1 <- read_tsv("data/UKDA-6614-tab/tab/us_w1/a_indresp.tab")
+w2 <- read_tsv("data/UKDA-6614-tab/tab/us_w2/b_indresp.tab")
