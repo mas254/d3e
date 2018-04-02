86eea679748b2f1cfc52be84dc3d4a648a07e71d
mas254 <mas254@exeter.ac.uk>
2018-04-02 10:59:53 +0100

    Loaded data

--- d3e.R
+++ d3e.R
@@ -1,1 +1,85 @@
 # Home copy of R work on d3e
+
+library(tidyverse)
+library(data.table)
+library('readr')
+library(readxl)
+library(reshape2)
+library('plyr')
+library("fmsb")
+library(arm)
+
+install.packages('data.table')
+install.packages('readr')
+install.packages('tidyverse')
+
+install.packages('reshape2')
+install.packages('plyr')
+install.packages('fmsb')
+install.packages('arm')
+
+files <- dir("data/UKDA-6614-tab/tab",
+             pattern="indresp",
+             recursive = TRUE,
+             full.names=TRUE)
+
+files <- files[str_detect(files, "us")]
+
+varsI <- c('pidp', 'qfhigh_dv', 'fimnnet_dv', 'fimngrs_dv', 'sclfsat1', 'dvage', 'sex', 'racel', 'racelo_code')
+
+for (i in 1:7) {
+  varsToSelectI <- paste(letters[i], varsI, sep = "_")
+  varsToSelectI <- c("pidp", varsToSelectI)
+  dataI <- fread(files[i], select = varsToSelectI)
+  if (i == 1) {
+    dI <- dataI  
+  }
+  else {
+    dI <- full_join(dI, dataI, by = "pidp")
+  }
+  rm(dataI)
+}
+
+write_tsv(dI, "myData/dI.tab")
+
+dmeltI <- dI %>%
+  melt(id = "pidp") %>%
+  separate(variable, into = c("wave", "variable"), sep = "_") %>%
+  dcast(pidp + wave ~ variable)
+
+dnaI <- dmeltI %>%
+  mutate(qfhigh = ifelse(qfhigh > 0, qfhigh, NA)) %>%
+  mutate(qfhigh = ifelse(qfhigh < 96, qfhigh, NA)) %>%
+  mutate(sclfsat1 = ifelse(sclfsat1 > 0, sclfsat1, NA)) %>%
+  mutate(dvage = ifelse(dvage > 0, dvage, NA)) %>%
+  mutate(sex = ifelse(sex > 0, sex, NA)) %>%
+  mutate(racel = ifelse(racel > 0, racel, NA)) %>%
+  mutate(racel = ifelse(racel < 96, racel, NA)) %>%
+  mutate(racelo = ifelse(racelo > 0, racelo, NA)) %>%
+  mutate(racelo = ifelse(racelo < 96, racelo, NA))
+
+dcleanI <- na.omit(dnaI) %>%
+  mutate(qfhigh = recode(qfhigh, "1" = "1-Higher degree", "2" = "2-Degree/equiv", "3" = "3-Higher ed",
+                         "4" = "3-Higher ed", "5" = "3-Higher ed", "6" = "3-Higher ed",
+                         "7" = "4-A level/equiv", "8" = "4-A level/equiv", "9" = "4-A level/equiv",
+                         "10" = "5-AS level/equiv", "11" = "5-AS level/equiv", "12" = "5-AS level/equiv",
+                         "13" = "6-GCSE/equiv", "14" = "7-CSE", "15" = "6-GCSE/equiv", "16" = "8-Other school cert")) %>%
+  mutate(qfhigh = factor(qfhigh)) %>%
+  mutate(sclfsat1 = recode(sclfsat1, "1" = "1-completely dissatisfied", "2" = "2-mostly dissatisfied",
+                           "3" = "3-somewhat dissatisfied", "4" = "4-neither satisfied or dissatisfied",
+                           "5" = "5-somewhat satisfied", "6" = "6-mostly satisfied", "7" = "7-completely satisfied")) %>%
+  mutate(sclfsat1 = factor(sclfsat1)) %>%
+  mutate(sex = recode(sex, "1" = "male", "2" = "female")) %>%
+  mutate(sex = factor(sex))
+
+dcontI <- na.omit(dnaI) %>%
+  mutate(qfhigh = recode(qfhigh, "1" = "1-Higher degree", "2" = "2-Degree/equiv", "3" = "3-Higher ed",
+                         "4" = "3-Higher ed", "5" = "3-Higher ed", "6" = "3-Higher ed",
+                         "7" = "4-A level/equiv", "8" = "4-A level/equiv", "9" = "4-A level/equiv",
+                         "10" = "5-AS level/equiv", "11" = "5-AS level/equiv", "12" = "5-AS level/equiv",
+                         "13" = "6-GCSE/equiv", "14" = "7-CSE", "15" = "6-GCSE/equiv", "16" = "8-Other school cert")) %>%
+  mutate(qfhigh = factor(qfhigh)) %>%
+  mutate(sex = recode(sex, "1" = "male", "2" = "female")) %>%
+  mutate(sex = factor(sex))
+
+rm(dI, dmeltI, dnaI, W1, W7)
