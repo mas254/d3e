4405b4d70c0c9c2462d803bc531bfe3b15d95d95
mas254 <mas254@exeter.ac.uk>
2018-04-02 11:08:44 +0100

    Up to date.

--- d3e.R
+++ d3e.R
@@ -1,32 +1,29 @@
 # Home copy of R work on d3e
 
-library(tidyverse)
+##### All Variables - Data Cleaning #####
+
+# Packages required
 library(data.table)
-library('readr')
-library(readxl)
-library(reshape2)
-library('plyr')
-library("fmsb")
-library(arm)
+library(readr)
+library(tidyverse)
 
-install.packages('data.table')
-install.packages('readr')
-install.packages('tidyverse')
-
-install.packages('reshape2')
-install.packages('plyr')
-install.packages('fmsb')
-install.packages('arm')
-
+# Firstly, we create a character vector with the full paths to the individual indresp files from all the waves.
 files <- dir("data/UKDA-6614-tab/tab",
              pattern="indresp",
              recursive = TRUE,
              full.names=TRUE)
 
+# This includes files from the BHPS that we don't need so we must drop them. We can use the function
+# str_detect() to identify files from the Understanding Society only and then keep only
+# their names in the vector "files".
 files <- files[str_detect(files, "us")]
 
+# Selecting variables
 varsI <- c('pidp', 'qfhigh_dv', 'fimnnet_dv', 'fimngrs_dv', 'sclfsat1', 'dvage', 'sex', 'racel', 'racelo_code')
 
+# pidp - ID number; qfhigh - highest qualification; fimnnet - monthly net personal income; sclfsat1 - wellbeing; dvage - age; sex - sex; racel - race
+
+# Create the data file with all 7 waves with the selected variables
 for (i in 1:7) {
   varsToSelectI <- paste(letters[i], varsI, sep = "_")
   varsToSelectI <- c("pidp", varsToSelectI)
@@ -40,13 +37,16 @@ for (i in 1:7) {
   rm(dataI)
 }
 
+# Saving this data in myData
 write_tsv(dI, "myData/dI.tab")
 
+# Cleaning the data so wave number is a variable and we can see the answers for each variable for each person with ease.
 dmeltI <- dI %>%
   melt(id = "pidp") %>%
   separate(variable, into = c("wave", "variable"), sep = "_") %>%
   dcast(pidp + wave ~ variable)
 
+# Changing negative and other 'unknown' values to NA
 dnaI <- dmeltI %>%
   mutate(qfhigh = ifelse(qfhigh > 0, qfhigh, NA)) %>%
   mutate(qfhigh = ifelse(qfhigh < 96, qfhigh, NA)) %>%
@@ -58,6 +58,24 @@ dnaI <- dmeltI %>%
   mutate(racelo = ifelse(racelo > 0, racelo, NA)) %>%
   mutate(racelo = ifelse(racelo < 96, racelo, NA))
 
+# Subsetting the data frame into individual waves
+W1 <- subset(dnaI, wave == 'a',
+             select = c(1:10))
+
+W7 <- subset(dnaI, wave == 'g',
+             select = c(1:10))
+
+# Checking for variables with larger than average number of NAs
+summary(W1)
+summary(W7)
+
+# As fimngrs and fimnnet are similar, we will use fimnnet, and as racel and racelo have almost double the NA values of any other variable for W7, we cut them
+dnaI$racel <- dnaI$racelo <- NULL
+dnaI <- subset(dnaI, , -c(racel, racelo, fimngrs))
+
+# Changing numerical responses to labels (this is to make our descriptive analysis easier to do, as well as making the results clearer).
+# We have also combined equivalent qualifications, so we have fewer variables, again making our descriptive analysis easier to do and
+# clearer to interpret.
 dcleanI <- na.omit(dnaI) %>%
   mutate(qfhigh = recode(qfhigh, "1" = "1-Higher degree", "2" = "2-Degree/equiv", "3" = "3-Higher ed",
                          "4" = "3-Higher ed", "5" = "3-Higher ed", "6" = "3-Higher ed",
@@ -72,6 +90,9 @@ dcleanI <- na.omit(dnaI) %>%
   mutate(sex = recode(sex, "1" = "male", "2" = "female")) %>%
   mutate(sex = factor(sex))
 
+# This is for treating the wellbeing score as a continuous variable
+# When we do our inferential analysis, we will need to treat "sclfsat1" as a continuous, not a factor variable,
+# thus using the numerical, rather than categorical, values.
 dcontI <- na.omit(dnaI) %>%
   mutate(qfhigh = recode(qfhigh, "1" = "1-Higher degree", "2" = "2-Degree/equiv", "3" = "3-Higher ed",
                          "4" = "3-Higher ed", "5" = "3-Higher ed", "6" = "3-Higher ed",
@@ -82,4 +103,24 @@ dcontI <- na.omit(dnaI) %>%
   mutate(sex = recode(sex, "1" = "male", "2" = "female")) %>%
   mutate(sex = factor(sex))
 
+# Removing unnecessary data files. These are taking up memory and will not be needed for the rest of the analysis.
 rm(dI, dmeltI, dnaI, W1, W7)
+
+
+##### Visualising Relationships #####
+# First, to explore our data, we will look at the distribution of wellbeing in each wave to see for any overall trends
+ggplot(dcleanI, aes(x = wave, fill = sclfsat1)) +
+  geom_bar(position = 'fill')
+
+# We are going to take a closer look at the first, middle and last waves of the data
+# Subsetting the data frame into waves 1, 4 and 7
+Wc <- subset(dcleanI, wave == c('a', 'd', 'g'),
+             select = c(1:7))
+
+# Visualising the spread of wellbeing in each wave
+ggplot(Wc, aes(x = wave, fill = sclfsat1)) +
+  geom_bar(position = 'fill')
+
+# Do average wellbeing score next (make and use data frame).
+
+# Then split by education and income (for individual waves).
