587d8d27d56c55e5fec8fb9680d8cc6d550e8244
mas254 <mas254@exeter.ac.uk>
2018-04-05 10:06:27 +0100

    Finalised.

--- Used for visualising.R
+++ Used for visualising.R
@@ -20,6 +20,8 @@ dnaI <- dmeltI %>%
   mutate(racelo = ifelse(racelo > 0, racelo, NA)) %>%
   mutate(racelo = ifelse(racelo < 96, racelo, NA))
 
+dnaI <- subset(dnaI, , -c(racel, racelo, fimngrs))
+
 dcleanI <- na.omit(dnaI) %>%
   mutate(qfhigh = recode(qfhigh, "1" = "1-Higher degree", "2" = "2-Degree/equiv", "3" = "3-Higher ed",
                          "4" = "3-Higher ed", "5" = "3-Higher ed", "6" = "3-Higher ed",
@@ -67,9 +69,11 @@ rm(dI, dmeltI, dnaI)
 ggplot(dcleanI, aes(x = year, fill = sclfsat1)) +
   geom_bar(position = 'fill')
 
+Wc <- subset(dcleanI, wave == c('a', 'd', 'g'),
+             select = c(1:7))
+
 ggplot(Wc, aes(x = wave, fill = sclfsat1)) +
-  geom_bar(position = 'fill') +
-  coord_polar(theta = 'y')
+  geom_bar(position = 'fill')
 
 dcontI %>%
   group_by(year) %>%
@@ -80,6 +84,14 @@ dcontI %>%
   geom_point() +
   geom_line()
 
+f <- subset(dcontI, sex == 'female')
+
+m <- subset(dcontI, sex == 'male')
+
+ggplot(dcleanI, aes(x = sex, fill = sclfsat1)) +
+  geom_bar(position = 'fill') +
+  coord_polar(theta = 'y')
+
 dcontI %>%
   group_by(year, sex) %>%
   summarise(
@@ -89,24 +101,24 @@ dcontI %>%
   geom_point() +
   geom_line()
 
-f1 <- subset(dcontI, sex == 'female')
+# http://www.sthda.com/english/wiki/ggplot2-violin-plot-quick-start-guide-r-software-and-data-visualization
 
-m1 <- subset(dcontI, sex == 'male')
-
-ggplot(f1, aes(x = wave, y = sclfsat1, fill = wave)) +
+a <- ggplot(f, aes(x = wave, y = sclfsat1, fill = wave)) +
   geom_violin()
 
-ggplot(m1, aes(x = wave, y = sclfsat1, fill = wave)) +
-  geom_violin(
+b <- ggplot(m, aes(x = wave, y = sclfsat1, fill = wave)) +
+  geom_violin()
 
 grid.newpage()
 
-pushViewport(viewport(layout = grid.layout(, 2)))
+pushViewport(viewport(layout = grid.layout(1, 2)))
 vplayout <- function(x, y) viewport(layout.pos.row = x, layout.pos.col = y)
 print(a, vp = vplayout(1, 1))
 print(b, vp = vplayout(1, 2))
 
 table(f$sclfsat1, f$wave)
+
+table(m$sclfsat1, m$wave)
 
 dcontI %>%
   group_by(year, qfhigh) %>%
@@ -116,6 +128,34 @@ dcontI %>%
   ggplot(aes(x = year, y = meanWb, colour = qfhigh)) +
   geom_point() +
   geom_line()
+
+ggplot(dcleanI, aes(x = qfhigh, fill = sclfsat1)) +
+  geom_bar(position = 'fill')
+
+Wd <- subset(dcleanI, qfhigh == c('1-Higher degree', '4-A level/equiv', '7-CSE'),
+             select = c(1:7))
+
+ggplot(Wd, aes(x = qfhigh, fill = sclfsat1)) +
+  geom_bar(position = 'fill') +
+  coord_polar(theta = 'y')
+
+High <- subset(dcleanI, qfhigh == '1-Higher degree',
+               select = c(wave, sclfsat1))
+
+A <- subset(dcleanI, qfhigh == '4-A level/equiv',
+            select = c(wave, sclfsat1))
+
+CSE <- subset(dcleanI, qfhigh == '7-CSE',
+              select = c(wave, sclfsat1))
+
+ggplot(High, aes(x = wave, fill = sclfsat1)) +
+  geom_bar(position = 'fill')
+
+ggplot(A, aes(x = wave, fill = sclfsat1)) +
+  geom_bar(position = 'fill')
+
+ggplot(CSE, aes(x = wave, fill = sclfsat1)) +
+  geom_bar(position = 'fill')
 
 c <- f %>%
   group_by(year, qfhigh) %>%
@@ -190,3 +230,39 @@ s %>%
   ggplot(aes(x = year, y = meanWb, colour = income)) +
   geom_point() +
   geom_line()
+
+sc <- dcleanI %>% 
+  filter(fimnnet > 0) %>% 
+  mutate(income = ifelse(fimnnet >= 1.506607e-04 & fimnnet <= 6.706440e+02, 20,
+                         ifelse(fimnnet > 6.706440e+02 & fimnnet <= 1.125058e+03, 40,
+                                ifelse(fimnnet > 1.125058e+03 & fimnnet <= 1.567589e+03, 60,
+                                       ifelse(fimnnet > 1.567589e+03 & fimnnet <= 2.200000e+03, 80,
+                                              ifelse(fimnnet > 2.200000e+03 & fimnnet <= 1.500000e+04, 100, NA)))))) %>% 
+  mutate(income = as.factor(income))
+
+ggplot(sc, aes(x = income, fill = sclfsat1)) +
+  geom_bar(position = 'fill')
+
+table(sc$sclfsat1, sc$income)
+
+hundred <- subset(sc, income == '100',
+               select = c(wave, sclfsat1))
+
+sixty <- subset(sc, income == '60',
+            select = c(wave, sclfsat1))
+
+twenty <- subset(sc, income == '20',
+              select = c(wave, sclfsat1))
+
+ggplot(High, aes(x = wave, fill = sclfsat1)) +
+  geom_bar(position = 'fill')
+
+ggplot(A, aes(x = wave, fill = sclfsat1)) +
+  geom_bar(position = 'fill')
+
+ggplot(CSE, aes(x = wave, fill = sclfsat1)) +
+  geom_bar(position = 'fill')
+
+table(hundred$sclfsat1, hundred$wave)
+table(sixty$sclfsat1, sixty$wave)
+table(twenty$sclfsat1, twenty$wave)
